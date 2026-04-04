4a130b90dff914cdd258f0c11f611f56cb93037f
Erdna's Kram <72553603+erdnaskram@users.noreply.github.com>
2026-04-04 02:37:43 +0200

    - entfernen Base Card - Entfernen Botstrapvue - entfernen ungenutzer Bilder - Hinzufügen neuer Bilder

--- Vue-3/src/main.js
+++ Vue-3/src/main.js
@@ -4,21 +4,14 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import './assets/global.css';
-import {createBootstrap} from 'bootstrap-vue-next'
 import { vuetify } from './plugins/vuetify';
 import '@mdi/font/css/materialdesignicons.css'
 
-// Add the necessary CSS
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-
 const app = createApp(App);
-const boostrap = createBootstrap();
 const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
-app.use(boostrap);
 app.use(vuetify);
 
 app.mount('#app');
